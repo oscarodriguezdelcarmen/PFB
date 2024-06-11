1bf9c3f0b410ca5c7c8e033e78a524504e918777
oscar <ordc120@gmail.com>
2024-06-11 20:41:35 +0200

    basico front

--- back/server.js
+++ back/server.js
@@ -1,17 +1,17 @@
-"use strict";
+'use strict';
 
-import dotenv from "dotenv";
-import express from "express";
+import dotenv from 'dotenv';
+import express from 'express';
 
 dotenv.config();
 const app = express();
 
 const { PORT } = process.env;
 
-app.get("/", (req, res) => {
-  res.send("Hola desde proyecto de fin de bootcamp");
+app.get('/', (req, res) => {
+    res.send('Hola desde proyecto de fin de bootcamp');
 });
 
 app.listen(PORT, () => {
-  console.log(`Server on fire 🔥🔥 on ${PORT}`);
+    console.log(`Server on fire 🔥🔥 on ${PORT}`);
 });
