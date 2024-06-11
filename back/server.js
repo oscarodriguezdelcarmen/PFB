3898492677b89a1a272084082cbb6e676d4720e4
oscar <ordc120@gmail.com>
2024-06-11 19:46:57 +0200

    API express básica

--- back/server.js
+++ back/server.js
@@ -1,3 +1,17 @@
 "use strict";
 
-console.log("ciao mondo!!");
+import dotenv from "dotenv";
+import express from "express";
+
+dotenv.config();
+const app = express();
+
+const { PORT } = process.env;
+
+app.get("/", (req, res) => {
+  res.send("Hola desde proyecto de fin de bootcamp");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server on fire 🔥🔥 on ${PORT}`);
+});
