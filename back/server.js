98ce16ae9fcd5899e8a9db31ca90b2142964902a
oscar <ordc120@gmail.com>
2024-06-11 21:04:06 +0200

    404

--- back/server.js
+++ back/server.js
@@ -12,6 +12,10 @@ app.get('/', (req, res) => {
     res.send('Hola desde proyecto de fin de bootcamp');
 });
 
+app.use((req, res) => {
+    res.status(404).send('Recurso no encontrado');
+});
+
 app.get('/login', (req, res) => {
     res.status(201).send('Login!!!');
 });
